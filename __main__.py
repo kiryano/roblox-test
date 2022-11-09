ae18ca8fe271645e43726237edd5caa7c211acfd
kiryano <97414481+kiryano@users.noreply.github.com>
2022-11-08 21:39:12 -0800

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,6 +1,8 @@
 from lightbulb.ext import tasks
 import hikari
 import lightbulb
+import aiohttp
+import httpx
 
 bot = lightbulb.BotApp(token='',
                        intents=hikari.Intents.ALL)
