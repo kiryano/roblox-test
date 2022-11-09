152712daa5b78886ef7e1323df2e6e3b22507bd5
kiryano <97414481+kiryano@users.noreply.github.com>
2022-11-08 21:38:52 -0800

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,8 +1,6 @@
 from lightbulb.ext import tasks
-import logging
 import hikari
 import lightbulb
-import random
 
 bot = lightbulb.BotApp(token='',
                        intents=hikari.Intents.ALL)
