36d3cd26cf50e2c38ed310ab3923f62152e3e6b5
kiryano <97414481+kiryano@users.noreply.github.com>
2022-11-08 21:38:29 -0800

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,12 @@
+from lightbulb.ext import tasks
+import logging
+import hikari
+import lightbulb
+from roblox import Client
+import miru
+import random
+
+bot = lightbulb.BotApp(token='',
+                       intents=hikari.Intents.ALL)
+
+bot.run()
